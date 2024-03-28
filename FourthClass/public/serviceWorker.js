885580e58e16b69a671ad5c43ab4375ed0e8b685
joaovtfor <joaovtfor@hotmail.com>
2024-03-28 15:13:54 -0300

    `Add` - Main files

---
+++ FourthClass/public/serviceWorker.js
@@ -0,0 +1,26 @@
+const staticPwaADS = "cache_web_app_upf";
+
+const assets = [
+    "/",
+    "/index.html",
+    "/main.css",
+    "/panda.png",
+    "/sea_turtle.jpg",
+    "/main.js",
+];
+
+self.addEventListener("install", installEvent => {
+    installEvent.waitUntil(
+        caches.open(staticPwaADS).then(cache => {
+            cache.addAll(assets);
+        })
+    )
+});
+
+self.addEventListener("fetch", fetchEvent => {
+    fetchEvent.respondWith(
+        caches.match(fetchEvent.request).then(res => {
+            return res || fetch(fetchEvent.request);
+        })
+    )
+});
