4b452d3e49959077197e8474be3c554e66698393
joaovtfor <joaovtfor@hotmail.com>
2024-04-08 21:22:31 -0300

    `Add` - SixthClass

---
+++ SixthClass/main.js
@@ -0,0 +1,8 @@
+var sum = function (a, b) {
+    return a + b;
+};
+// function sum(a: number, b: number): number {
+//     return a + b;
+// };
+var result = sum(2, 7);
+console.log("Result: ", result);
---
+++ SixthClass/main.ts
@@ -0,0 +1,10 @@
+const sum = (a:number, b:number): number => {
+    return a + b;
+};
+
+// function sum(a: number, b: number): number {
+//     return a + b;
+// };
+
+const result: number = sum(2,7);
+console.log("Result: ", result);
